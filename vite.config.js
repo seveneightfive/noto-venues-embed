7c18c50229da90039df7940bf4166133f280a314
seveneightfive <35607452+seveneightfive@users.noreply.github.com>
2025-11-14 01:28:21 -0600

    Refactor Vite config to simplify build options

--- vite.config.js
+++ vite.config.js
@@ -3,20 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'process.env': {}
-  },
   build: {
     outDir: 'dist',
-    lib: {
-      entry: 'src/embed-standalone.jsx',
-      name: 'NOTOVenuesEmbed',
-      fileName: 'noto-venues-embed',
-      formats: ['iife']
-    },
     rollupOptions: {
-      output: {
-        inlineDynamicImports: true
+      input: {
+        main: './index.html'
       }
     }
   }
